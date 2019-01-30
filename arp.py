3338ab601cd7516a8e50bda73250ee0ba9d16260
collen.roller <collen.roller@us.af.mil>
2019-01-30 13:16:02 -0500

    Initial push of completed ARP dataset with Python wrapper complete! :D

---
+++ arp.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import ast
+import re
+import sys
+
+class ARP:
+    
+    dataset_files = {"countries":"processed_itu_countries_regex.csv",
+                     "organizations":"processed_itu_organizations_regex.csv"}
+    
+    def __init__(self):
+        self.datasets_loaded = False
+        self.load_datasets(self.dataset_files)
+            
+    def load_datasets(self, files):
+        try:
+            if files is not None:
+                self.dfs = {}
+                for key, f in files.items():
+                    self.dfs[key] = pd.read_csv(f)
+                self.datasets_loaded = True
+            else:
+                self.print_dataset_error()        
+        except:
+            self.print_dataset_error()
+            
+    def parse(self, callsign): 
+        res = []
+        if not self.datasets_loaded:
+            self.load_datasets(self.dataset_files)
+        else:
+            #parse the data
+            for key, df in self.dfs.items():
+                for index, entry in df.iterrows():
+                    result = re.match(entry['regex'], callsign)
+                    if result is not None:
+                        #print("Success!")
+                        #print(entry)
+                        res.append({"nation":entry["nation"],"description":entry["description"],"iso codes":entry["iso codes"]})
+                        
+        return res
+    
+    def print_dataset_error(self):
+        print("Can't identify dataset, try loading it manually")
+        print("Use load_dataset(<tags.csv>)")
+
+if __name__ == "__main__":
+    arp = ARP()
+    for tail in sys.argv[1:]:
+        print("%s: %s" % (tail,arp.parse(tail)))
