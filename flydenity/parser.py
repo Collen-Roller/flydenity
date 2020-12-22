0342d3d16b5cab07e37849d94f61715064acbdd5
Konstantin Gründger <kammermark@gmx.de>
2020-12-22 12:07:41 +0100

    Add the ability to parse ICAO 24bit registrations

--- flydenity/parser.py
+++ flydenity/parser.py
@@ -21,15 +21,16 @@ DATASET_FILES = {
 
 class ARParser():
     def __init__(self):
-        # read the input files into the callsign mapping: callsign -> [data1, data2, ...]
-        self.callsigns = {}
+        # read the input files into mappings:
+        self.callsigns = {}     # the callsign mapping: callsign -> [data1, data2, ...]
+        self.icao24bit = {}     # the icao24bit mapping: icao24bit_prefix -> data
         for dataset_type in DATASET_FILES:
             with open(os.path.join(CURRENT_PATH, DATASET_FILES[dataset_type])) as csvfile:
                 csvreader = csv.reader(csvfile)
                 header = next(csvreader)
                 for row in csvreader:
                     raw_data = {key: value for (key, value) in zip(header, row)}
-                    
+
                     data = {}
                     if 'nation' in raw_data:
                         data['type'] = 'country'
@@ -42,12 +43,15 @@ class ARParser():
                         data['name'] = raw_data['name']
                     else:
                         raise ValueError(f"Input file for {dataset_type} '{DATASET_FILES[dataset_type]}' is corrupt.")
-                    
+
                     data['description'] = raw_data['description']
                     data['priority'] = raw_data['priority']
                     data['callsigns'] = [callsign[1:-1] for callsign in raw_data['callsign'][1:-1].split(', ')]
                     data['suffixes'] = [suffix[1:-1] for suffix in raw_data['suffix'][1:-1].split(', ')]
                     data['regex'] = re.compile(raw_data['regex'])
+                    data['icao24bit_from'] = raw_data['icao24bit_from']
+                    data['icao24bit_to'] = raw_data['icao24bit_to']
+                    data['icao24bit_prefixes'] = [prefix[1:-1] for prefix in raw_data['icao24bit_prefix'][1:-1].split(', ')]
 
                     strict_regex = raw_data['regex'].replace('-{0,1}', '\-').replace('{0,1}$', '$')
                     data['strict_regex'] = re.compile(strict_regex)
@@ -58,10 +62,27 @@ class ARParser():
                         else:
                             self.callsigns[callsign].append(data)
 
+                    for prefix in data['icao24bit_prefixes']:
+                        self.icao24bit[prefix] = data
+
         self.min_callsign_len = min([len(callsign) for callsign in self.callsigns.keys()])
         self.max_callsign_len = max([len(callsign) for callsign in self.callsigns.keys()])
 
-    def parse(self, string, strict=False):
+    def _best_match_to_dict(self, best_match):
+        if best_match['type'] == 'country':
+            return {
+                    'nation': best_match['nation'],
+                    'description': best_match['description'],
+                    'iso2': best_match['iso2'],
+                    'iso3': best_match['iso3']
+                }
+        elif best_match['type'] == 'organization':
+            return {
+                'name': best_match['name'],
+                'description': best_match['description']
+            }
+
+    def _parse_registration(self, string, strict):
         # find the datasets matching with the string
         datasets = []
         for callsign_len in range(self.min_callsign_len, self.max_callsign_len+1):
@@ -87,23 +108,30 @@ class ARParser():
             best_matches = matches_by_priority[max(matches_by_priority.keys())]
             if len(best_matches) > 1:
                 print(f"Warning: multiple matches with same priority found")
-            
-            best_match = best_matches[0]
-            if best_match['type'] == 'country':
-                return {
-                        'nation': best_match['nation'],
-                        'description': best_match['description'],
-                        'iso2': best_match['iso2'],
-                        'iso3': best_match['iso3']
-                    }
-            elif best_match['type'] == 'organization':
-                return {
-                    'name': best_match['name'],
-                    'description': best_match['description']
-                }
+
+            return self._best_match_to_dict(best_matches[0])
 
         # return None if the string doesn't match with any of the datasets
         else:
             return None
 
+    def _parse_icao24bit(self, string, strict):
+        if strict and not re.match('[0-9A-F]{6}', string):
+            print(f"Warning: ICAO 24bit must be hexadecimal with length of 6 chars")
+            return None
 
+        best_match = None
+        for prefix in [string[0:i+1] for i in range(len(string))]:
+            if prefix in self.icao24bit:
+                best_match = self.icao24bit[prefix]
+
+        if best_match is None:
+            return None
+        else:
+            return self._best_match_to_dict(best_match)
+
+    def parse(self, string, strict=False, icao24bit=False):
+        if icao24bit is False:
+            return self._parse_registration(string, strict)
+        else:
+            return self._parse_icao24bit(string, strict)
